3db1f34d33acf73dfbbf107385757cd3d9e74a81
Rick Halterman <halterman@southern.edu>
2024-08-30 18:17:48 -0400

    Version: Fri Aug 30 18:17:47 EDT 2024

---
+++ 2024-08-30/input.py
@@ -0,0 +1,8 @@
+name = input('Please enter your name: ')
+print(name, 'nice to meet you')
+
+num1 = int(input('Please enter an integer: '))
+num2 = int(input('Please enter another integer: '))
+
+
+print('The sum of', num1, 'and', num2, 'is', num1 + num2 )
